8306510fc8dc0e5d7d82405ff25c8697856bb1a7
04-tech <75111353+04-tech@users.noreply.github.com>
2021-07-28 21:09:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+     var menuToggle=document.querySelector('.toggle');
+     var banner=document.querySelector('.banner');
+     banner.classList.toggle('active');
+     menuToggle.classList.toggle('active');
+}
